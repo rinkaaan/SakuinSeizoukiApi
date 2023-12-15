d89e3a7b77171e77762effcaa72f2ce97b6d0411
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2023-12-14 20:52:21 -0800

    Add validate_app_data_directory

--- api/resources/settings.py
+++ api/resources/settings.py
@@ -1,14 +1,16 @@
+import os
 from threading import Thread
 
 import webview
 from apiflask import APIBlueprint, Schema
+from apiflask.fields import String, Boolean
 
 settings_bp = APIBlueprint("Settings", __name__, url_prefix="/settings")
 
 
 @settings_bp.post("/app-data-directory")
 @settings_bp.output({})
-def post():
+def select_app_data_directory():
     def run():
         from api.app import socketio
         window = webview.active_window()
@@ -21,3 +23,20 @@ def post():
     t.start()
 
     return ""
+
+
+class AppDataDirectoryValidateIn(Schema):
+    app_data_directory = String()
+
+
+class AppDataDirectoryValidateOut(Schema):
+    valid = Boolean()
+
+
+@settings_bp.post("/app-data-directory/validate")
+@settings_bp.input(AppDataDirectoryValidateIn, arg_name="data")
+@settings_bp.output(AppDataDirectoryValidateOut)
+def validate_app_data_directory(data):
+    return {
+        "valid": os.path.isdir(data["app_data_directory"])
+    }
