a7e9153781fc7c94b72eed05acf3a9e73abca4fc
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2023-12-14 17:16:00 -0800

    Add sqlite

--- api/app.py
+++ api/app.py
@@ -6,6 +6,8 @@ from api.resources.book_index import book_index_bp
 from api.resources.pdf import pdf_bp
 from api.resources.settings import settings_bp
 from api.resources.time import time_bp
+from models.base import Base
+from nguylinc_python_utils.sqlalchemy import init_sqlite_db
 
 flask_app = APIFlask(__name__, title="SakuinSeizouki API", version="0.1.0", spec_path="/openapi.yaml")
 socketio = SocketIO(flask_app, cors_allowed_origins="*")
@@ -18,6 +20,8 @@ flask_app.register_blueprint(time_bp)
 flask_app.register_blueprint(book_index_bp)
 flask_app.register_blueprint(pdf_bp)
 flask_app.register_blueprint(settings_bp)
+
+session = init_sqlite_db(Base)
 
 
 @socketio.on("connect")
